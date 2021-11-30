e2b85518f22245b53b650678211765bfe1f433fb
SCP002 <feedusfeedme@gmail.com>
2021-12-01 01:47:31 +0300

    Replace boilerplate test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('contains the game class', () => {
+  const { container } = render(<App />);
+  expect(container.classList.contains('game')).toBe(true);
 });
