985cbc8c5af543455c95afc67adc324159ce4fbd
SCP002 <feedusfeedme@gmail.com>
2021-12-01 02:00:46 +0300

    Use absolute paths

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import App from './App';
+import App from 'App';
 
 test('contains the game class', () => {
   const { container } = render(<App />);
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css';
+import 'App.css';
 
 function App() {
-  return (
-    <div className="App">It's working</div>
-  );
+  return <div className="App">It's working</div>;
 }
 
 export default App;
