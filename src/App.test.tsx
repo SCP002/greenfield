48c32c346ffc401505b89047c972f301a0aa74bc
SCP002 <feedusfeedme@gmail.com>
2021-12-01 01:54:22 +0300

    Simplify main test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,5 +3,5 @@ import App from './App';
 
 test('contains the game class', () => {
   const { container } = render(<App />);
-  expect(container.classList.contains('game')).toBe(true);
+  expect(container).toHaveClass('game');
 });
