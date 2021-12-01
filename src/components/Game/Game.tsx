20f3368320d15007b23f62190415d5e5d58de48b
SCP002 <feedusfeedme@gmail.com>
2021-12-02 01:00:30 +0300

    Add initial state to Game component,

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import Field from 'components/Field/Field';
+import { Component } from 'react';
 import './Game.scss';
 
-export default class Game extends React.Component {
+interface Props {}
+
+interface State {
+  changeClickedCell: boolean;
+  columns: number;
+  rows: number;
+  stepsAmount: number;
+}
+
+export default class Game extends Component<Props, State> {
+  public constructor(props: Props) {
+    super(props);
+    this.state = {
+      changeClickedCell: false,
+      columns: 6,
+      rows: 1,
+      stepsAmount: 0,
+    };
+  }
+
   public render(): JSX.Element {
-    return <div>Game component</div>;
+    return (
+      <div>
+        <Field></Field>
+      </div>
+    );
   }
 }
