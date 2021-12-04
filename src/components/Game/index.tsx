7b24f20172b597f0351d9948da36a7eb1148f0b3
SCP002 <feedusfeedme@gmail.com>
2021-12-04 16:37:21 +0300

    Add field property to state of Game component

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,4 +1,6 @@
-import Field from 'components/Field';
+import { Field as FieldComp } from 'components/Field';
+import { Field } from 'components/Game/Field';
+import { Row } from 'components/Game/Row';
 import 'components/Game/styles.scoped.scss';
 import { Component } from 'react';
 
@@ -7,26 +9,44 @@ interface Props {}
 interface State {
   changeClickedCell: boolean;
   columns: number;
+  field?: ReadonlyArray<Row>;
   rows: number;
   stepsAmount: number;
 }
 
-export default class Game extends Component<Props, State> {
+export class Game extends Component<Props, State> {
   public constructor(props: Props) {
     super(props);
+
+    const cols = 6;
+    const rows = 1;
     this.state = {
       changeClickedCell: false,
-      columns: 6,
-      rows: 1,
+      columns: cols,
+      field: Field.New(rows, cols),
+      rows: rows,
       stepsAmount: 0,
     };
+  }
+
+  public componentDidUpdate(
+    prevProps: Readonly<Props>,
+    prevState: Readonly<State>
+  ) {
+    console.log(prevState); // TODO: Remove; Update Field on columns / rows change
   }
 
   public render(): JSX.Element {
     return (
       <div className={Game.name}>
-        <Field />
+        <FieldComp />
       </div>
     );
+  }
+
+  private resetSteps() {
+    this.setState({
+      stepsAmount: 0,
+    });
   }
 }
