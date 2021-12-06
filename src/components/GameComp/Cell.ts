35a3016ca3e7d6613268756d5b41a095c0afa73f
SCP002 <feedusfeedme@gmail.com>
2021-12-06 23:25:49 +0300

    Only hold 'initialActive' state in Cell.ts

--- src/components/GameComp/Cell.ts
+++ src/components/GameComp/Cell.ts
@@ -1,17 +1,9 @@
-import produce from 'immer';
-
 export interface Cell {
-  readonly active: boolean;
+  readonly initialActive: boolean;
 }
 
 export namespace Cell {
   export function New(): Cell {
-    return { active: Math.random() >= 0.5 };
-  }
-
-  export function revertState(cell: Cell): Cell {
-    return produce(cell, (c) => {
-      c.active = !c.active;
-    });
+    return { initialActive: Math.random() >= 0.5 };
   }
 }
