b844a8c9c35c464a36c9aeefed84cbe51d38e691
SCP002 <feedusfeedme@gmail.com>
2021-12-07 00:32:12 +0300

    Use Cell component isActive prop to compose className

--- src/components/CellComp/index.tsx
+++ src/components/CellComp/index.tsx
@@ -1,9 +1,15 @@
-import 'components/Cell/styles.scoped.scss';
+import 'components/CellComp/styles.scoped.scss';
 
 interface Props {
+  isActive: boolean;
   onClick: () => void;
 }
 
 export function CellComp(props: Props): JSX.Element {
-  return <button className={CellComp.name} onClick={props.onClick} />;
+  return (
+    <button
+      className={`${CellComp.name} ${props.isActive ? 'active' : ''}`}
+      onClick={props.onClick}
+    />
+  );
 }
