b60dd8f18bd9d76d3548ca90334027a9287abd87
SCP002 <feedusfeedme@gmail.com>
2021-12-07 00:33:02 +0300

    Fix row array fills with cells of the same state

--- src/components/FieldComp/Field.ts
+++ src/components/FieldComp/Field.ts
@@ -1,16 +1,19 @@
 import { Cell } from 'components/FieldComp/Cell';
 import { Row } from 'components/FieldComp/Row';
+import lo from 'lodash';
 
 export namespace Field {
   export function New(
     colAmount: number,
     rowAmount: number
   ): ReadonlyArray<Row> {
-    const cells = Array<Cell>(colAmount);
-    cells.fill(Cell.New());
+    const cells = lo.map(Array(colAmount), () => {
+      return Cell.New();
+    });
 
-    const rows = Array<Row>(rowAmount);
-    rows.fill(Row.New(cells));
+    const rows = lo.map(Array(rowAmount), () => {
+      return Row.New(cells);
+    });
 
     return rows;
   }
