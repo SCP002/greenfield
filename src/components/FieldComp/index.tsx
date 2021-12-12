42815739426b988bdc0b1d2cc237c6ac74e245ef
SCP002 <feedusfeedme@gmail.com>
2021-12-13 02:04:14 +0300

    Change field size depending on props in FieldComp

--- src/components/FieldComp/index.tsx
+++ src/components/FieldComp/index.tsx
@@ -1,6 +1,8 @@
 import { CellComp } from 'components/CellComp';
 import { Field } from 'components/FieldComp/Field';
+import { Row } from 'components/FieldComp/Row';
 import 'components/FieldComp/styles.scoped.scss';
+import { useEffect } from 'react';
 import { useImmer } from 'use-immer';
 
 interface Props {
@@ -8,10 +10,20 @@ interface Props {
   rowAmount: number;
 }
 
+interface State {
+  rows: Row[];
+}
+
 export function FieldComp(props: Props): JSX.Element {
-  const [rows, updateRows] = useImmer(
-    Field.New(props.colAmount, props.rowAmount)
-  );
+  const [state, updateState] = useImmer<State>({
+    rows: Field.New(props.colAmount, props.rowAmount),
+  });
+
+  useEffect(() => {
+    updateState((draft) => {
+      draft.rows = Field.New(props.colAmount, props.rowAmount);
+    });
+  }, [props, updateState]);
 
   return (
     <div className={FieldComp.name}>
@@ -22,13 +34,13 @@ export function FieldComp(props: Props): JSX.Element {
   );
 
   function onCellClick(rowIdx: number, cellIdx: number) {
-    updateRows((rows) => {
-      Field.revertAreaState(rows, cellIdx, rowIdx);
+    updateState((draft) => {
+      Field.revertAreaState(draft.rows, cellIdx, rowIdx);
     });
   }
 
   function renderRows(): JSX.Element[] {
-    return rows.map((row, rowIdx) => {
+    return state.rows.map((row, rowIdx) => {
       return (
         <tr key={rowIdx}>
           {row.cells.map((cell, cellIdx) => {
