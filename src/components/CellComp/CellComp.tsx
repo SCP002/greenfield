7827d97041477c12efc307731e762585fe166960
SCP002 <feedusfeedme@gmail.com>
2021-12-30 10:43:26 +0300

    Remove redundant observer wrapper in CellComp

--- src/components/CellComp/CellComp.tsx
+++ src/components/CellComp/CellComp.tsx
@@ -1,12 +1,11 @@
 import 'components/CellComp/CellComp.scoped.scss';
-import { observer } from 'mobx-react-lite';
 
 interface Props {
   isActive: boolean;
   onClick: (cellIdx?: number, rowIdx?: number) => void;
 }
 
-export const CellComp = observer(function CellComp(props: Props) {
+export function CellComp(props: Props) {
   return (
     <button
       className={`${CellComp.name} ${props.isActive ? 'active' : ''}`}
@@ -15,4 +14,4 @@ export const CellComp = observer(function CellComp(props: Props) {
       }}
     />
   );
-});
+};
